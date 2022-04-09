77f485de5f0125eb05a7191dd9ce632650e33700
Satyajeetsinh <satyajeetsinh98@gmail.com>
2022-04-09 15:16:00 +0530

    created project structure

---
+++ lib/modules/dashboard_view/dashboard_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DashBoardView extends StatefulWidget {
+  const DashBoardView({Key? key}) : super(key: key);
+
+  @override
+  State<DashBoardView> createState() => _DashBoardViewState();
+}
+
+class _DashBoardViewState extends State<DashBoardView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('dashboard'),
+      ),
+    );
+  }
+}
